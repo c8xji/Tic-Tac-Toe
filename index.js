53df549db1a3246e9982c3d1aa69691ba2725b80
Derrick Tsou <rex14725@gmail.com>
2025-03-21 19:34:22 +1100

    complete game logic and UI

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let circle = '<img src="./imgs/circle.svg" alt="o" class="placed">';
 let cross = '<img src="./imgs/x-lg.svg" alt="x" class="placed">';
+let player1Score = 0;
+let player2Score = 0;
 let isCircle = true;
 let gameEnd = false;
 let board = [
@@ -24,24 +26,39 @@ $(".box").click(function() {
     if (isCircle) {
         $(this).append(circle);
         board[row][col] = "O";
+        $("h2").text("It's X's turn.");
     } else {
         $(this).append(cross);
         board[row][col] = "X";
+        $("h2").text("It's O's turn.");
     }
 
     if (checkWinner(isCircle ? "O" : "X")) {
-        alert((isCircle ? "O" : "X") + " WIN!");
+        $("h2").text((isCircle ? "O" : "X") + " WIN!");
+        getPoint();
+        $(".scoreO").text(player1Score);
+        $(".scoreX").text(player2Score);
         gameEnd = true;
         return;
     }
 
     if (checkDraw()) {
-        alert("DRAW!");
+        $("h2").text("DRAW!");
         gameEnd = true;
         return;
     }
     isCircle = !isCircle;
 });
+
+$("#newGame").click(function() {
+    restartGame();
+});
+
+$("#reset").click(function() {
+    reset();
+});
+
+
 
 function checkWinner(player) {
     for (let i = 0; i < 3; i++) {
@@ -56,4 +73,33 @@ function checkWinner(player) {
 
 function checkDraw() {
     return board.flat().every(cell => cell !== "");
+}
+
+function restartGame() {
+    gameEnd = false;
+    board = [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""]
+    ];
+    isCircle = true;
+    $(".box").empty();
+    $("h2").text("Who can be the winner?");
+}
+
+function reset() {
+    restartGame();
+    player1Score = 0;
+    player2Score = 0;
+    $(".scoreO").text(player1Score);
+    $(".scoreX").text(player2Score);
+    $("h2").text("Who can be the winner?");
+}
+
+function getPoint() {
+    if (isCircle) {
+        player1Score += 1;
+    } else {
+        player2Score += 1;
+    }
 }
